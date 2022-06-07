f5d7dcda1e99fbedfe0f28991db133e68a130e1b
Connor Gawley (Desktop) <connorgawley@uvic.ca>
2022-06-07 15:12:22 -0600

    homepage made and styled, may revisit

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,4 +1,3 @@
-import { randomInt } from 'crypto'
 import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar'
 import './index.scss'
@@ -10,23 +9,33 @@ const Home = () => {
     const [i, setI] = useState<number>(1)
     useEffect(() => {
         function getI() {
-            setI(1)
+            setI(Math.floor(Math.random() * 3) + 1)
         }
         async function getBackground(index: number) {
-            const res = await fetch(`${baseURL}home-background?i=1`)
-            console.log(res)
+            const res = await fetch(`${baseURL}home-background?i=${i}`)
             setBackground(res.url)
         }
         getI()
+        console.log(i)
         getBackground(i)
     }, [])
 
     return (
         <div id='Home'>
-            <Navbar />
-            <div>
-                <img src={background} alt='' className='background' />
+            <div className='content'>
+                <div className='text-container'>
+                    <div className='text'>
+                        <h1>
+                            Welcome to <span>Brawlhalla Randomizer</span>
+                        </h1>
+                        <h3>The better way to explore and choose your favourite Legend!</h3>
+                    </div>
+                </div>
+                <div className='background'>
+                    <img src={background} alt='' className='background-img' />
+                </div>
             </div>
+            <Navbar />
         </div>
     )
 }
